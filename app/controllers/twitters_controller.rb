6645df0e2c12ec35049b6c87858029e9d55ceb9e
sagae keiga <sagae5.28rujeae@gmail.com>
2017-03-02 09:05:36 +0000

    twitter

--- app/controllers/twitters_controller.rb
+++ app/controllers/twitters_controller.rb
@@ -1,6 +1,8 @@
 class TwittersController < ApplicationController
+  
   def index
   end
+
 
   def show
   end
@@ -12,5 +14,34 @@ class TwittersController < ApplicationController
   end
 
   def destroy
+  end
+  
+  def crawl
+    config = {
+    	:consumer_key => 'HxTbIelBlbjp56cERilNt6XEy',
+    	:consumer_secret => 'mzrDSM4WkFC23cx1TRQPrQ4p2AT0Yt1RxYpojAcD5Ua7RZZPWA',
+    	:access_token => '732909452563619845-ptSOidEhCvXBPGCWUF6KoOqrFRcJAJ4',
+    	:access_token_secret => 'x7m24zjdrJyhe1w8dmt5paIPEgfnSTmrCjLXJ8CnASjQd'
+    }
+    
+    client = Twitter::REST::Client.new(config)
+    
+    @tweets = []
+    client.user_timeline('dkfj').each {|tweet|
+    	# Tweet時間
+    	puts tweet.created_at
+    
+    	# Tweet本文
+    	@tweets = tweet.text
+    
+    	# Retweet数
+    	puts "Retweetされた数 : " + tweet.retweet_count.to_s
+    
+    	# お気に入りされた数
+    	puts "お気に入りされた数 : " + tweet.favorite_count.to_s
+    
+    	# 位置情報
+    	puts "位置情報 : " + tweet.geo if !tweet.geo.nil?
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'twitters/index'
+  root 'twitters#index'
 
   get 'twitters/show'
 
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'twitters/edit'
 
   get 'twitters/destroy'
+  
+  get 'twitters/crawl'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
