5f0cae508442879ad2859cc8a18c4434e793f046
sagae keiga <sagae5.28rujeae@gmail.com>
2017-06-16 12:35:05 +0000

    d

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,12 @@
 worker_processes 2
 working_directory "/home/solacom_test/work/terror/current"
 
-listen "/var/run/unicorn/unicorn_machine_learning.sock"
-pid "/var/run/unicorn/unicorn_machine_learning.pid"
+listen "/var/run/unicorn/terror.sock"
+pid "/var/run/unicorn/terror.pid"
 
 
-stderr_path "/home/anime/machine_learning/current/log/unicorn.log"
-stdout_path "/home/anime/machine_learning/current/log/unicorn.log"
+stderr_path "/home/solacom_test/work/terror/current/log/unicorn.log"
+stdout_path "/home/solacom_test/work/terror/current/log/unicorn.log"
 
 
 preload_app true
