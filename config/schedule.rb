60f307750b1699610b9e5e96893be34470d6f7e1
sagae keiga <sagae5.28rujeae@gmail.com>
2017-06-17 13:56:44 +0000

    d

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,8 +7,6 @@
 #
 set :output, '/log/crontab.log'
 set :environment, :production
-env :PATH, ENV['PATH']
-job_type :rbenv_rake, %q!eval "$(rbenv init -)"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output!
 
 every 1.minute do
   rake 'terror:sample RAILS_ENV=production'
